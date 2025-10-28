bf4d7b6a2d7171e777446aa7cd330d43568cc1e8
Afwan-Rezal <21b6027@ubd.edu.bn>
2025-10-28 09:50:16 +0800

    Updated Seeder for Testing Purpose

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,22 +19,46 @@ class UserSeeder extends Seeder
                 'username' => 'admin',
                 'email' => 'admin@mail.com',
                 'password' => bcrypt('12345678'), 
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 2,
                 'username' => 'staff',
                 'email' => 'staff@mail.com',
                 'password' => bcrypt('12345678'),
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 3,
                 'username' => '21B6027',
                 'email' => '21B6027@ubd.edu.bn',
                 'password' => bcrypt('12345678'),
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
         ]);
 
         DB::table('user_profiles')->insert([
+            [
+                'user_id' => 1,
+                'full_name' => 'Abdullah Admin',
+                'role' => 'Admin',
+                'gender' => 'M',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            
+            [
+                'user_id' => 2,
+                'full_name' => 'Ahmad Staff',
+                'role' => 'Staff',
+                'gender' => 'M',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],  
+            
             [
                 'user_id' => 3,
                 'full_name' => 'Afif Afwan Bin Mohamad Rezal',
