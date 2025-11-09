f42cf0ab0660128826d4271845d8860d3e6c6872
Afwan-Rezal <afwan.rezal@gmail.com>
2025-11-10 00:40:42 +0800

    Introduced mail functionality for booking approvals. This will ensure that users receive email notifications when their bookings are approved.

---
+++ resources/views/mail/booking_approved_mail.blade.php
@@ -0,0 +1,21 @@
+<h2>Your room booking, {{ $mailData['room'] }} has been approved!</h2>
+
+<p>Hello, {{ $mailData['name'] }}!</p>
+<p>We are pleased to inform you that your booking for the room <strong>{{ $mailData['room'] }}</strong> has been approved.</p>
+
+<br><br>
+
+<p><strong>Booking Details:</strong></p>
+<ul>
+    <li><strong>Room:</strong> {{ $mailData['room'] ?? 'N/A' }}</li>
+    <li><strong>Date:</strong> {{ $mailData['date'] ?? 'N/A' }}</li>
+    <li><strong>Time:</strong> {{ $mailData['time_slot'] ?? $mailData['time'] ?? 'N/A' }}</li>
+</ul>
+
+<br><br>
+
+<p>Should you require to make changes to your booking, please change it <strong>within 30 minutes before the start time</strong>.</p>
+
+<p>Thank you for using our booking system.</p>
+<p>Best regards,<br>
+The SDS Booking Team</p>
