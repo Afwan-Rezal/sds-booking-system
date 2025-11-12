c6e147f33d646232626d4a1215e382037f161e24
Afwan-Rezal <afwan.rezal@gmail.com>
2025-11-12 22:11:51 +0800

    New CSS, updated 2/3 dashboards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> @yield('title') | {{config('app.name'), 'SDS Booking System'}}</title>
+    <title> @yield('title') | {{ config('app.name', 'SDS Booking System') }}</title>
     <link rel="icon" type="image/png" href="{{ asset('images/ubd_logo.png') }}">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/sds_booking_system.css') }}">
 </head>
 
 <body>
